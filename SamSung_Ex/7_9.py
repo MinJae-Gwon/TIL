602ac56cd4c6dafb75d41b4e5e589a1d221af901
MinJae Gwon <minjae2271@gmail.com>
2019-01-09 12:23:16 +0900

    D06 190109 AM | problems

---
+++ SamSung_Ex/7_9.py
@@ -0,0 +1,30 @@
+# b = 0
+# s = 7
+
+# for i in range(4):
+#     print(' '*b+'*'*s+' '*b)
+#     b+=1
+#     s-=2
+
+# b = 4
+# s=1
+# for i in range(5):
+#     print(' '*b+'*'*s)
+#     b-=1
+#     s+=1
+
+# l=[]
+# for i in range(1,101,2):
+#     l.append(str(i))
+# res = ', '.join(l)
+# print(res)
+
+
+f=82
+c=(f-32)*(5/9)
+print(f'{f:0.2f} ℉ => {c:0.2f} ℃')
+
+
+
+
+    
